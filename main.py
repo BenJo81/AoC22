490d6e30aedfd4ac1adf94f12616804bc50962e8
Ben Joseph <bjoseph@sourceallies.com>
2023-12-17 19:47:00 -0600

    2nd half

--- main.py
+++ main.py
@@ -3,29 +3,28 @@ with open("input.txt") as f:
 
 grand_total = 0
 game_dict = {}
-red_cubes = 12
-green_cubes = 13
-blue_cubes = 14
-game_over = False
 
 for game in games:
     which_game = game.split(":")
     game_dict[which_game[0]] = which_game[1]
 
 for x in range(len(game_dict)):
-    game_over = False
+    largest_blue = 0
+    largest_red = 0
+    largest_green = 0
     game_results = game_dict[f"Game {x+1}"].split(";")
     for game in game_results:
         colors = game.split(",")
         for color in colors:
-            if "blue" in color and int(color[1:3]) > blue_cubes:
-                game_over = True
-            elif "green" in color and int(color[1:3]) > green_cubes:
-                game_over = True
-            elif "red" in color and int(color[1:3]) > red_cubes:
-                game_over = True
-    print(game_over)
-    if not game_over:
-        grand_total += x+1
+            if "blue" in color and int(color[1:3]) > largest_blue:
+                largest_blue = int(color[1:3])
+            elif "green" in color and int(color[1:3]) > largest_green:
+                largest_green = int(color[1:3])
+            elif "red" in color and int(color[1:3]) > largest_red:
+                largest_red = int(color[1:3])
+        print(largest_green, largest_red, largest_blue)
+    semi_total = (largest_red * largest_green * largest_blue)
+    print(semi_total)
+    grand_total += semi_total
 
 print(grand_total)
